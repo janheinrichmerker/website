dbaf908f7845731b4cc1bdd6f0475de0fab60198
Heinrich Reimer <heinrich@heinrichreimersoftware.com>
2017-02-05 17:13:45 +0100

    Updated styles

--- res/js/scripts.js
+++ res/js/scripts.js
@@ -8,34 +8,11 @@ $(function() { //Document is ready
     });
     
     //Close drawer when user clicks a link
-    $(".mdl-layout__drawer .mdl-navigation__link").click(function() {
-        $(".mdl-layout").get(0).MaterialLayout.toggleDrawer();
-    });
-
-    //Contact form
-    var contactForm = $("#contact_form");
-    var contactSnackbar = $("#contact_snackbar");
-    contactForm.submit(function(event) {
-        $.post("res/php/contact.php", contactForm.serialize())
-            .done(function(data) {
-                contactForm.find(":input").attr("disabled", true);
-                var button = contactForm.find("button[name='submit']");
-                button.html(button.data("success"));
-                contactSnackbar.get(0).MaterialSnackbar.showSnackbar({
-                    message: data,
-                    timeout: 4000
-                });
-            })
-            .fail(function(jqXHR) {
-                contactSnackbar.get(0).MaterialSnackbar.showSnackbar({
-                    message: jqXHR.responseText,
-                    timeout: 4000
-                });
-            });
-        event.preventDefault();
+    $("app-drawer paper-item").click(function () {
+        $("app-drawer").get(0).close();
     });
 });
 
 function scrollTo(reference) {
-    $(".mdl-layout__content").scrollTo(reference);
+    $("body").scrollTo(reference);
 }
