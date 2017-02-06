9b51fea655ab2cd4530c15986f246ac2809e2dfc
Heinrich Reimer <heinrich@heinrichreimersoftware.com>
2017-02-06 16:15:00 +0100

    Updated layout

--- res/js/jquery.scrollto.js
+++ res/js/jquery.scrollto.js
@@ -1,15 +1,18 @@
 $.fn.scrollTo = function( target, options, callback ){
     if(typeof options == 'function' && arguments.length == 2){ callback = options; options = target; }
     var settings = $.extend({
-        scrollTarget  : target,
+        scrollTarget: target,
         offsetTop: 60,
-        duration      : 500,
-        easing        : 'swing'
+        duration: 500,
+        easing: 'swing'
     }, options);
     return this.each(function(){
         var scrollPane = $(this);
         var scrollTarget = (typeof settings.scrollTarget == "number") ? settings.scrollTarget : $(settings.scrollTarget);
         var scrollY = (typeof scrollTarget == "number") ? scrollTarget : scrollTarget.offset().top + scrollPane.scrollTop() - parseInt(settings.offsetTop);
+        console.log("scrollPane.scrollTop(): " + scrollPane.scrollTop());
+        console.log("scrollTarget.offset().top: " + scrollTarget.offset().top);
+        console.log("scrollY: " + scrollY);
         scrollPane.animate({scrollTop : scrollY }, parseInt(settings.duration), settings.easing, function(){
             if (typeof callback == 'function') { callback.call(this); }
         });
